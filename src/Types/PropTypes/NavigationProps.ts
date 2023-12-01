13eefcf4d510f95c76e19bed4a448a6af590ea00
Microsft-Wrd <mrjacobbrooks@gmail.com>
2023-12-01 00:19:12 +0000

    Navigation Types

---
+++ src/Types/PropTypes/NavigationProps.ts
@@ -0,0 +1,10 @@
+export interface INavProp {
+    id: number;
+    Name: string;
+    Endpoint: string;
+}
+
+export default interface NavigationPropList {
+    NavigationPropList: Array<INavProp>
+}
+
