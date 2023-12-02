8384dedba6514a179e26ca9b2793642f01409653
Microsft-Wrd <mrjacobbrooks@gmail.com>
2023-12-02 02:14:12 +0000

    Creating Input, for Cities
    - This input will sendAnotherWeatherRequest which will get data according to what the userinput is

--- src/Components/Weather/Weather.tsx
+++ src/Components/Weather/Weather.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import WeatherAPI from '../../Server/WeatherAPI'
+import React, { FormEvent } from 'react'
+import WeatherAPI from '../../Server/APIs/WeatherAPI'
 import { WeatherResponse } from '../../Types/WeatherTypes'
 
 const Weather = () => {
@@ -9,8 +9,6 @@ const Weather = () => {
     const weatherApi = React.useMemo(() => new WeatherAPI(), [])
 
     React.useEffect(() => {
-
-
         const fetchedData = async () => {
             try{
 
@@ -21,17 +19,32 @@ const Weather = () => {
             }catch(error){
                 console.error('Error fetching data: ', error)
             }
-
         }
-        
         fetchedData()
     }, [weatherApi])
+
+
+    const input = (data: FormEvent) => {
+        data.preventDefault()
+        const dataInput = new FormData(data.target as HTMLFormElement)
+        const dataInputString = dataInput.get('locationInput')
+        weatherApi.sendWeatherRequest(dataInputString as string)
+    }
 
     return (
         <div className='flex items-center justify-center h-[80vh] flex-col'>
             <div className='bg-gray-400 p-[25vh] rounded-full'>
                 <h1 className='font-extrabold text-center text-2xl'>Weather Statistics</h1>
-                <div className='text-center'>
+
+
+                <form className="flex flex-col justify-center text-center bg-slate-300 rounded-md p-2 m-2" onSubmit={(e) => input(e)}>
+                    <label className="mb-2" htmlFor="locationInput">Enter Location Here</label>
+                    <input className="mb-2 rounded-md text-center bg-slate-400 text-white placeholder:text-white" id="locationInput" type="text" placeholder='Location' name='locationInput'/>
+                    <input className="mb-2 hover:bg-slate-500 p-2 cursor-pointer bg-slate-300 rounded-xl" type="submit" value="Submit Location" />
+                </form>
+
+
+                <div className='text-center text-xl'>
                     <div>
                         <p>City: {weatherApi.getRegion()}</p>
                     </div>
