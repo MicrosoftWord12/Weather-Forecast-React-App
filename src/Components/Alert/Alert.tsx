ebee6aa3df5d4b0336b8435e81227aaac347e03e
Microsft-Wrd <mrjacobbrooks@gmail.com>
2023-12-04 23:40:57 +0000

    Re-Wrote Alert HTML

--- src/Components/Alert/Alert.tsx
+++ src/Components/Alert/Alert.tsx
@@ -1,13 +1,23 @@
 import { IAlertProps } from "../../Types/ComponentProps"
 
-const Alert = ({ hidden }: IAlertProps) => {
-    if(!hidden){
+const Alert = ({ isShowing, message }: IAlertProps) => {
+
+    if(message == null || message === ""){
+        message = "There is No Message"
+    }
+
+    if(isShowing){
         return (
-            <div className="bg-gray-400"></div>
+            <div className="flex flex-col items-center text-center bg-slate-400 p-4 m-4 rounded-md">
+                <div className="bg-slate-500 p-5 rounded-lg">
+                    <div className="text-2xl text-red-500 font-bold">Alert</div>
+                    <div className="text-xl">{message}</div>
+                </div>
+            </div> 
         )
     }else{
         return (
-            <div className='hidden'></div>
+            <></>
         )
     }
 }
